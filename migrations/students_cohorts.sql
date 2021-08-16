3774add5ce19d74468c72c00be5212ec1649e2b0
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-16 15:41:32 -0400

    Add the seeds directory

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,17 +1,20 @@
+DROP TABLE IF EXISTS cohorts CASCADE;
+DROP TABLE IF EXISTS students;
 CREATE TABLE cohorts (
-    id int PRIMARY KEY,
-    name text,
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
     start_date DATE,
     end_date DATE
 );
 
+
 CREATE TABLE students (
-    id int PRIMARY KEY,
-    name text,
-    email text,
-    phone text,
-    github text,
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    github VARCHAR(255),
     start_date DATE,
     end_date DATE,
-    cohort_id INT
+    cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE 
 );
