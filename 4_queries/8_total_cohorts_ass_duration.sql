ef2014ee276918b5f8610fb918d4818b701e87f7
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-17 15:35:41 -0400

    Get the total duration of all assistance requests for each cohort.

---
+++ 4_queries/8_total_cohorts_ass_duration.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort,
+SUM(assistance_requests.completed_at - assistance_requests.started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY SUM(assistance_requests.completed_at - assistance_requests.started_at);
